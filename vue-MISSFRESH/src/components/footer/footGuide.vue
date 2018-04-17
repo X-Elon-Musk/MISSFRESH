573b0a9ec05b741f443bfc72822be7f759af5947
792884274 <792884274@qq.com>
2018-04-17 10:28:57 +0800

    update

--- vue-MISSFRESH/src/components/footer/footGuide.vue
+++ vue-MISSFRESH/src/components/footer/footGuide.vue
@@ -2,19 +2,19 @@
 	<div>
         <ul class="foot-nav">
         	<li class="nav-item" @click="goAddress({path: '/msite'})">
-        		<span class="nav-icon nav-home"></span>
+        		<span class="nav-icon nav-home" :style="{backgroundImage:$route.path.indexOf('msite')==-1?`url(${backgroundImage.home})`:`url(${backgroundImage.homeActive})`}"></span>
         		<p class="nav-label">首页</p>
         	</li>
         	<li class="nav-item" @click="goAddress({path: '/search/geohash'})">
-        		<span class="nav-icon nav-newvp"></span>
+        		<span class="nav-icon nav-newvp" :style="{backgroundImage:$route.path.indexOf('search')==-1?`url(${backgroundImage.newvp})`:`url(${backgroundImage.newvpActive})`}"></span>
         		<p class="nav-label">会员</p>
         	</li>
         	<li class="nav-item" @click="goAddress({path: '/order'})">
-        		<span class="nav-icon nav-cart"></span>
+        		<span class="nav-icon nav-cart" :style="{backgroundImage:$route.path.indexOf('order')==-1?`url(${backgroundImage.cart})`:`url(${backgroundImage.cartActive})`}"></span>
         		<p class="nav-label">购物车</p>
         	</li>
-        	<li class="nav-item" @click="goAddress({path: '/order'})">
-        		<span class="nav-icon nav-mine"></span>
+        	<li class="nav-item" @click="goAddress({path: '/profile'})">
+        		<span class="nav-icon nav-mine" :style="{backgroundImage:$route.path.indexOf('profile')==-1?`url(${backgroundImage.mine})`:`url(${backgroundImage.mineActive})`}"></span>
         		<p class="nav-label">我的</p>
         	</li>
         </ul>
@@ -24,7 +24,16 @@
 	export default{
 		data(){
 			return {
-
+				backgroundImage:{
+					home: require('src/images/home.png'),
+					newvp: require('src/images/newvp.png'),
+					cart: require('src/images/cart.png'),
+					mine: require('src/images/mine.png'),
+					homeActive: require('src/images/home-active.png'),
+					newvpActive: require('src/images/newvp-active.png'),
+					cartActive: require('src/images/cart-active.png'),
+					mineActive: require('src/images/mine-active.png')
+				}
 			}
 		},
 		methods: {
@@ -58,21 +67,23 @@
 			    width: 27px;
 			    height: 27px;
 			    margin: 0 auto;
+			    background-size: 100% 100%; 
+			    background-repeat: no-repeat;
 	    	}
 	    	.nav-home{
-	    		background: url(src/images/home.png) no-repeat;
+	    		// background: url(src/images/home.png) no-repeat;
 	    		background-size: 100% 100%; 
 	    	}
 	    	.nav-newvp{
-	    		background: url(src/images/newvp.png) no-repeat;
+	    		// background: url(src/images/newvp.png) no-repeat;
 	    		background-size: 100% 100%; 
 	    	}
 	    	.nav-cart{
-	    		background: url(src/images/cart.png) no-repeat;
+	    		// background: url(src/images/cart.png) no-repeat;
 	    		background-size: 100% 100%; 
 	    	}
 	    	.nav-mine{
-	    		background: url(src/images/mine.png) no-repeat;
+	    		// background: url(src/images/mine.png) no-repeat;
 	    		background-size: 100% 100%; 
 	    	}
 	    	.nav-label{
--- vue-MISSFRESH/src/page/order/order.vue
+++ vue-MISSFRESH/src/page/order/order.vue
@@ -1,15 +1,19 @@
 <template>
 	<div>
-		hello world!!
+		<foot-guide></foot-guide>
 	</div>
 </template>
 <script>
+	import footGuide from 'src/components/footer/footGuide'
 	export default{
 		data(){
 			return {
 
 			}
-		}
+		},
+		components:{
+	        footGuide
+	    },
 	}
 </script>
 <style>
