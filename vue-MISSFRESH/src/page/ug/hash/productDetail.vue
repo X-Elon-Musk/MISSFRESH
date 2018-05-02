cafc6367b8972eaaff969b313a229634a97dc718
792884274 <792884274@qq.com>
2018-05-03 01:27:53 +0800

    update

--- vue-MISSFRESH/src/page/ug/hash/productDetail.vue
+++ vue-MISSFRESH/src/page/ug/hash/productDetail.vue
@@ -2,7 +2,7 @@
     <div class="product-detail">
     	<div class="clearfix section-header">
     		<span class="header-text">商品详情</span>
-    		<strong class="f_r share-icon"></strong>
+    		<strong class="share-icon"></strong>
     	</div>
     	<div class="product-img-container">
     		<carousel></carousel>
@@ -48,14 +48,14 @@
     			<div class="padding_common share-volume">已经有<span>1172</span>人分享了该商品</div>
     		</li>
     	</ul>
-    	<div class="padding_common buyer-said">
+    	<div class="buyer-said">
     		<div class="buyer-area-person">
-    			<div class="flexbox">
-    				<i class="buyer-area-person-tip"></i>
-    				<div class="buyer-area-person-icon">
+    			<div class="clearfix flexbox">
+    				<div class="f_l buyer-area-person-icon">
     					<img src="~src/images/person-icon.png" alt="">
     				</div>
-    				<span class="buyer-name">摩奇复活核心团队</span>
+    				<span class="f_l buyer-name">摩奇复活核心团队</span>
+    				<i class="f_r buyer-area-person-tip"></i>
     			</div>
     			<div class="buyer-person-des">
     				由每日优鲜商品中心VP刘啸峰发起，得到摩奇董事长姜建民，摩奇饮料工艺师张昱的大力支持。
@@ -155,6 +155,9 @@
 				}
 			}
 			.share-icon{
+				position: absolute;
+				right: 0;
+				top: 0;
 				.bg(36px,36px,#fff,'~src/images/icon/share.png',1.4em);
 			}
 		}
@@ -249,36 +252,60 @@
 					}
 				}
 				.share-volume{
-					padding: 0.5em 3% 2em;
+					padding: 0.5em 3% 4em;
 					color: @color_gray;
 				}
 			}
 		}
 		.buyer-said{
+			margin: 1em 3% 2em;
+		    box-sizing: border-box;
 			.buyer-area-person{
+				box-shadow: 0 0.125rem 0.5rem 0 #E7E7E7;
+			    border-radius: 0.375rem;
+			    padding: 0.4em 3%;
+			    position: relative;
 				.flexbox{
-					.buyer-area-person-tip{
-
-					}
+					margin-top: -2em;
+					overflow: hidden;
+					height: 4em;
 					.buyer-area-person-icon{
+						margin-left: 0.5em;
+						.wh(4em,4em);
+						border-radius: 100%;
 						img{
-
+							display: inline-block;
+							.wh(100%,100%);
 						}
 					}
 					.buyer-name{
-
+						margin-left: 0.5em;
+						.font(4em,0.8em,@color_common);
+						font-weight: 700;
+					}
+					.buyer-area-person-tip{
+						.bg(3em,100%,transparent,'~src/images/icon/say.png',3em);
 					}
 				}
 				.buyer-person-des{
-
+					.font(1.5em,0.8em,@color_common);
+					padding-top: 0.4em;
+					padding-bottom: 0.4em;
 				}
 				.buyer-description{
+					padding: 0.5em 1em;
+					box-sizing: border-box;
+					border-radius: 5px;
+					background-color: #F5F5F5;
 					.marks-left{
-
+						.bg(100%,1em,transparent,'~src/images/icon/quotation.png',1.4em);
+						background-position: left center;
 					}
 					.commodity-details{
 						li{
-
+							.font(1.4em,0.8em,@color_common);
+							padding-bottom:0.5em;
+							padding-top: 0.5em; 
 						}
 					}
 				}
