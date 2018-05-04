9c1d0bf3d7d5264bb55e05f983707e233869f01c
792884274 <792884274@qq.com>
2018-05-04 18:08:52 +0800

    update

---
+++ node-MISSFRESH/model/sql-config.js
@@ -0,0 +1,21 @@
+var mysql=require('mysql');
+var connection=mysql.createConnection({
+	host: '39.104.65.227',
+	user: 'root',
+	password: 'Beijing@0983',
+	database: 'missfresh'
+})
+
+connection.connect();
+//操作数据库
+exports.mysql=function (sql,params,callback) {
+	connection.query(sql,params,function (err,result) {
+		if (err) {
+			callback(err,null);
+			return;
+		} else{
+			callback(null,result);
+		}		
+	})
+	// connection.end();
+}
