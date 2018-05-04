3c57593c61fddf7d712fa3b4ecf5e5a5d616326c
792884274 <792884274@qq.com>
2018-05-04 18:25:02 +0800

    update

--- node-MISSFRESH/app.js
+++ node-MISSFRESH/app.js
@@ -21,56 +21,6 @@ app.use(express.static("./public",{
 
 
 
-
-
-/*//显示首页(登录页面)
-app.get('/',router.index);
-//登录
-app.post('/login',router.login);
-//显示注册页面
-app.get('/noteRegister',router.noteRegister);
-//注册
-app.post('/register',router.register);
-//显示备忘录
-app.get('/notePages',router.notePages);
-//获取个人所有备忘录文本
-// app.get('/noteNotes',router.noteNotes);
-app.get('/noteNotes/:id?',router.noteNotes);
-//搜索备忘录
-app.post('/search',router.search);
-//获取用户信息页面
-app.get('/noteUser',router.noteUser);
-//显示修改用户名页面
-app.get('/noteUsername',router.noteUsername);
-//修改用户的用户名
-app.post('/noteChangeUsername',router.noteChangeUsername);
-//显示修改用户密码页面
-app.get('/notePassword',router.notePassword);
-//修改用户的密码
-app.post('/noteChangePassword',router.noteChangePassword);
-//显示修改用户手机号页面
-app.get('/noteTelephone',router.noteTelephone);
-//用户绑定手机号
-app.post('/bindTelephone',router.bindTelephone);
-//显示搜索页面
-app.get('/noteSearch',router.noteSearch);
-//显示编辑页面
-app.get('/noteEdit/:id?',router.noteEdit);
-//发表备忘记录
-app.post('/record/:id?',router.record);
-//删除单条备忘记录
-app.post('/delete/:id?',router.delete);
-//获取图片验证码
-app.get('/captcha',router.captcha);
-//获取手机验证码
-app.post('/teleCode',router.teleCode);
-//第三方登录
-//QQ第三方登录
-app.get('/sign',router.sign);
-//退出登录
-app.get('/signOut',router.signOut);
-// app.get('/signOut',router.notePages);*/
-
 app.get('/test',router.test);
 
 app.listen(3390);
