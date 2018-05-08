b0bae4732462c32aa1a3c7e0b631553d2bb286a8
792884274 <792884274@qq.com>
2018-05-08 20:31:19 +0800

    update

---
+++ node-MISSFRESH/0508.js
@@ -0,0 +1,34 @@
+buy_permission:0
+cart_btn_name:"加入购物车"
+//cart_image:"https://j-image.missfresh.cn/img_20170425134548759.png"
+//image:"https://image.missfresh.cn/product_images/EA86A9F0D32F84B7CCB3321F6742AD65.JPG"
+//name:"维他柠檬茶6盒装"
+nationwide:0
+pao:0
+product_tags:[{name: "红包不可用",…}]
+promoteLevel:0
+promote_tag:"https://j-image.missfresh.cn/img_20180302175509409.png"
+promote_tag_new:""
+seckill_limit:0
+//sell_out:false
+//sku:"p-HBwtnmc-250ml*6"
+//stock:4783
+//subtitle:"港真 喝了它小心会上瘾哦（新老包装随机发货）"
+supplier:null
+type:"product"
+vip_price_pro:{price_up: {name: "", show_type: 2, color: 9868950, price: 1690},…}
+vip_product:0
+
+
+
+vip_price_pro:{price_up: {name: "", show_type: 2, color: 9868950, price: 1690},…}
+	price_down:{name: "", show_type: 1, color: 16730257, price: 1290}
+		color:16730257
+		name:""
+		price:1290
+		show_type:1
+	price_up:{name: "", show_type: 2, color: 9868950, price: 1690}
+		color:9868950
+		name:""
+		price:1690
+		show_type:2
