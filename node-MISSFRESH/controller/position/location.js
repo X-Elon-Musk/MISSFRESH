5a19f709190f614e9e8acf9f24717a987e60b2ff
792884274 <792884274@qq.com>
2018-05-17 19:32:24 +0800

    update

--- node-MISSFRESH/controller/position/location.js
+++ node-MISSFRESH/controller/position/location.js
@@ -1,60 +1,23 @@
 'use strict';
-/*import Mysql from '../model/sql-config.js'
-import Sql_product from '../model/sql-product.js'
-import formidable from 'formidable'
-import md5 from '../model/md5.js'
-
-var mysql=Mysql.mysql_;*/
-
-
-/*var Location=function () {};
-Location.prototype={
-	constructor: Location,
-	async location(req, res, next){
-		// var result = await mysql(Sql_product.selectAll());
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-	    console.log('result');
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-		// res.type('application/json');
-	    // res.jsonp(result);
-	    await con();
-	    res.send('result');
-	},
-	async con(req, res, next){
-		// var result = await mysql(Sql_product.selectAll());
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-	    console.log('result1');
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-		// res.type('application/json');
-	    // res.jsonp(result);
-	    res.send('result1');
-	}
-}
-export default new Location();*/
 
  class Location{
 	constructor(){
-		this.conss=this.conss.bind(this);
 		this.location=this.location.bind(this);
+		this.getIp=this.getIp.bind(this);
 	}
 	async location(req, res, next){
-		// var result = await mysql(Sql_product.selectAll());
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-	    console.log('result');
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-		// res.type('application/json');
-	    // res.jsonp(result);
-	    await this.conss();
-	    res.send('result');
+	    let clientIp = await this.getIp(req);  
+	    //console.log('客户端ip',clientIp)  
+	    res.send(clientIp);  
 	}
-	async conss(req, res, next){
-		// var result = await mysql(Sql_product.selectAll());
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-	    console.log('result1');
-	    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-		// res.type('application/json');
-	    // res.jsonp(result);
-	    // res.send('result1');
+	async getIp(req, res, next){
+		var ip = req.headers['x-real-ip'] ||  
+	        req.headers['x-forwarded-for'] ||  
+	        req.socket.remoteAddress || '';  
+	    if(ip.split(',').length>0){  
+	        ip = ip.split(',')[0];  
+	    }  
+	    return ip;
 	}
 }
 
