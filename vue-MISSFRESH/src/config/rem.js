4c4732df29a20c1f7c862709b60afede6b9ac2b5
792884274 <792884274@qq.com>
2018-07-06 16:21:05 +0800

    update

--- vue-MISSFRESH/src/config/rem.js
+++ vue-MISSFRESH/src/config/rem.js
@@ -4,9 +4,8 @@
         recalc = function() {
             let clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            // 16为默认字体大小，320为设计图宽度，320/16为设计稿rem值。屏幕宽度/设计稿rem宽度=页面动态font-size值
-            // docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
-            docEl.style.fontSize = clientWidth/(320/16) + 'px';
+            // 16为默认字体大小，320为设计图宽度，375/16为设计稿rem值。屏幕宽度/设计稿rem宽度=页面动态font-size值
+            docEl.style.fontSize = clientWidth/(375/16) + 'px';
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
